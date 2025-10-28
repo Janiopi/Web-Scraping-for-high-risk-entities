3562250d65277441c78e716a90feaa900b0c6202
janiopi <janio072003@hotmail.com>
2025-10-28 12:36:46 -0500

    fix: Change directory of Chrome installation in Dockerfile backend

--- backend/src/config/puppeteerConfig.js
+++ backend/src/config/puppeteerConfig.js
@@ -19,8 +19,19 @@ export const getPuppeteerConfig = () => {
         '--disable-background-timer-throttling',
         '--disable-backgrounding-occluded-windows',
         '--disable-renderer-backgrounding',
+        '--user-data-dir=/tmp/chrome-user-data',
+        '--disable-extensions',
+        '--disable-plugins',
+        '--disable-default-apps',
+        '--disable-sync',
+        '--disable-translate',
+        '--hide-scrollbars',
+        '--mute-audio',
+        '--no-default-browser-check',
+        '--no-pings',
+        '--window-size=1366,768',
       ],
-      // Try different Chrome paths for Render
+      // Use Chrome from system
       executablePath:
         process.env.PUPPETEER_EXECUTABLE_PATH ||
         '/usr/bin/google-chrome-stable',
